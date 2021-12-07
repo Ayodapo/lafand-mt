4bc9f5aeeac66e5ab6274712918e94ef29aa3649
unknown <ayodeleawokoya@gmail.com>
2021-12-07 19:43:54 +0100

    initial commit

--- joeytrainer/buildvocab.sh
+++ joeytrainer/buildvocab.sh
@@ -2,6 +2,6 @@ src=en
 tgt=zu
 #mkdir -p joeynmt/data/frmos/fr_mos/10k/
 
-python joeynmt/scripts/build_vocab.py ./joeytrainer/data/enzu/spdata/10k/JW/train.$src ./joeytrainer/data/enzu/spdata/10k/JW/train.$tgt --output_path ./joeytrainer/data/enzu/spdata/10k/JW/vocab.txt
+python joeynmt/scripts/build_vocab.py ./data/enzu/spdata/10k/JW/train.$src ./data/enzu/spdata/10k/JW/train.$tgt --output_path ./data/enzu/spdata/10k/JW/vocab.txt
 
-python joeynmt/scripts/build_vocab.py ./joeytrainer/data/enzu/spdata/20k/JW/train.$src ./joeytrainer/data/enzu/spdata/20k/JW/train.$tgt --output_path ./joeytrainer/data/enzu/spdata/20k/JW/vocab.txt
+python joeynmt/scripts/build_vocab.py ./data/enzu/spdata/20k/JW/train.$src ./data/enzu/spdata/20k/JW/train.$tgt --output_path ./data/enzu/spdata/20k/JW/vocab.txt
--- joeytrainer/createconfig.sh
+++ joeytrainer/createconfig.sh
@@ -1,5 +1,5 @@
 #create config file to train a model over the JW300 en-yo corpus using 10k sp vocabulary 
-python scripts/createconfig.py --source_lang en --target_lang zu --task 10kjw300enzu --model_path  ./joeytrainer/joeynmt/ --data_path ./joeytrainer/data/enzu/spdata/10k/JW/
+python scripts/createconfig.py --source_lang en --target_lang zu --task 10kjw300enzu --model_path  ./joeynmt/ --data_path ./data/enzu/spdata/10k/JW/
 #create config file to train a model over the JW300 en-yo corpus using 10k sp vocabulary
 #python scripts/createconfig.py --source_lang yo --target_lang en --task 10kjw300yoen --model_path  /netscratch/alabi/data/Lafand/joeytrainer/joeynmt/ --data_path /netscratch/alabi/data/Lafand/joeytrainer/data/enyo/spdata/10k/JW/
 
